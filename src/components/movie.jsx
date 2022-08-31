8b35c9097b378c2009340ae6522cc05a46e2a508
schulz-franco <fschulz2222@gmail.com>
2022-08-31 20:41:30 -0300

    added media section

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -10,21 +10,25 @@ export default function Movie() {
     const {id} = useParams()
     const [movie, set_movie] = useState(null)
     const [credits, set_credits] = useState(null)
+    const [images, set_images] = useState(null)
     const [loading, set_loading] = useState(true)
 
     useEffect(()=> {
         get_movie(id, "").then(res=> {
             set_movie(res)
-            console.log(res)
         })
         get_movie(id, "/credits").then(res=> {
             set_credits(res)
             console.log(res)
         })
+        get_movie(id, "/images?include_image_language=en").then(res=> {
+            set_images(res)
+            console.log(res)
+        })
         if (loading) {
             setTimeout(()=> {
                 set_loading(false)
-            }, 3000)
+            }, 4000)
         }
     }, [id])
 
@@ -40,7 +44,7 @@ export default function Movie() {
                 <div style={(movie.backdrop_path != "") && {backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(https://image.tmdb.org/t/p/original" + movie.backdrop_path + ")"}} className="header-container">
                     <img className="poster" src={"https://image.tmdb.org/t/p/w200" + movie.poster_path} alt={movie.title} />
                     <div className="title-container">
-                        <span className="title">{movie.title} {(movie.release_date != "") ? ("(" + movie.release_date.slice(0, 4) + ")") : ""}</span>
+                        <span className="title">{movie.title}</span>
                         <div className="vote-container">
                             <AiFillStar className="star"/>
                             <span className="vote">{movie.vote_average.toFixed(1)}/10</span>
@@ -55,10 +59,12 @@ export default function Movie() {
                             <span>‣ {(movie.runtime != 0) ? (Math.trunc(movie.runtime/60) + "h " + movie.runtime%60 + "m") : "Without duration"}</span>
                         </div>
                         <span>{(movie.genres.length != 0) && movie.genres.map((genre, index)=> {
-                            if (index != (movie.genres.length - 1)) {
-                                return genre.name + ", "
+                            if (index < 4) {
+                                if (index != (movie.genres.length - 1) && index < 3) {
+                                    return genre.name + ", "
+                                }
+                                return genre.name
                             }
-                            return genre.name
                         })}</span>
                     </div>
                     <div className="texts-container">
@@ -78,18 +84,38 @@ export default function Movie() {
                             })}
                         </div>
                     </div>
-                    {(credits) && 
-                        <div className="cast-container">
+                    {(credits && credits.cast.length != 0) && 
+                        <div className="section-container">
                             <span className="title">Top Billed Cast</span>
                             <div className="items">
                                 {credits.cast.map((person, index) => {
                                     if (index < 9) {
                                         return(
                                             <div className="item">
-                                                {(person.profile_path) ? <img src={"https://image.tmdb.org/t/p/original" + person.profile_path} alt={person.original_name} /> : <img src={No_img} alt="Without image" />}
+                                                {(person.profile_path) ? <img className="img-person" src={"https://image.tmdb.org/t/p/original" + person.profile_path} alt={person.original_name} /> : <img className="img-person" src={No_img} alt="Without image" />}
                                                 <Link className="name" to={"/person/" + person.id}>{person.original_name}</Link>
                                                 <span className="job">{person.character}</span>
                                             </div>
+                                        )
+                                    }
+                                })}
+                            </div>
+                            <Link className="full-crew" to={"/crew/" + movie.id} >Full Cast & Crew</Link>
+                        </div>
+                    }
+                    {(images && (images.backdrops.length != 0 || images.posters.length != 0)) && 
+                        <div className="section-container">
+                            <span className="title">Media</span>
+                            <div className="items">
+                                {(images.backdrops.length != 0) && images.backdrops.map(image => {
+                                    return(
+                                        <img width={300} height={200} className="img-backdrop" src={"https://image.tmdb.org/t/p/original" + image.file_path} alt={movie.title}/>
+                                    )
+                                })}
+                                {(images.posters.length != 0) && images.posters.map((image, index) => {
+                                    if (index < 10) {
+                                        return(
+                                            <img width={300} height={200} className="img-backdrop" src={"https://image.tmdb.org/t/p/original" + image.file_path} alt={movie.title}/>
                                         )
                                     }
                                 })}
